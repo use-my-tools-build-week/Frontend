13e43db00a445b91b85d4c5c36bae57fad18c492
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-17 14:36:28 -0400

    add redux dev tools

--- tools/src/index.js
+++ tools/src/index.js
@@ -4,21 +4,23 @@ import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './Reducer/Reducer';
 
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    reducer, applyMiddleware(thunk)
-)
+  reducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 
 
 ReactDOM.render(
 <Router>
  <Provider store={store}>
-  <App />  
+  <App />
  </Provider>
 </Router>
 
